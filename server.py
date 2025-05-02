54df40d930a50191e7a81d190c806e329f765bc9
Alvalen <alvalen.shafel04@gmail.com>
2025-05-02 23:03:25 +0700

    Refactor server.py

--- server.py
+++ server.py
@@ -12,7 +12,9 @@ def emotion_detector_route():
     return render_template("index.html", result=result, text=text_to_analyze)
 
 
-
 @app.route("/")
 def render_index_page():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
